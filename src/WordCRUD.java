578eb913eb21fd53af229a5daa2c1a353b0c2cee
LeeOnYu <loy7153@gmail.com>
2023-09-04 22:37:58 +0900

    listAll

--- src/WordCRUD.java
+++ src/WordCRUD.java
@@ -150,4 +150,10 @@ public class WordCRUD implements ICRUD {
         int level = s.nextInt();
         listAll(level);
     }
+
+    public void seachWord() {
+        System.out.print("=> 원하는 단어는? ");
+        String keyword = s.next();
+        listAll(keyword);
+    }
 }
